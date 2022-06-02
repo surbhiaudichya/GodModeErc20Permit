2b0c9ba3839ba1c800056f37cbf505ed7749d86b
Surbhi Audichya <audichyasurbhi@gmail.com>
2022-06-02 22:34:42 +0530

    Revert "Merge pull request #7 from surbhiaudichya/test/merge" yes
    This reverts commit 1f869c1b8658bf571199c05a25247e360ad0fef0, reversing
    changes made to f68cdf13a26747b9e5bd1ee32bb45000a54f1ad8.

--- contracts/GodModeErc20Permit.sol
+++ contracts/GodModeErc20Permit.sol
@@ -40,6 +40,4 @@ contract GodModeErc20Permit is Erc20, Erc20Permit {
     function __godMode_mint(address beneficiary, uint256 mintAmount) external {
         mintInternal(beneficiary, mintAmount);
     }
-    // commit 1
-    // commit 2
 }
